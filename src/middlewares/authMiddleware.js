58ee7f8fc3cdd0878424160e59cdefbd15044588
Chandnibai04 <chandnirathi04@gmail.com>
2026-02-04 15:34:38 +0500

    Fix model paths

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
+import User from "../models/User.model.js";
 
 export const authenticate = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
